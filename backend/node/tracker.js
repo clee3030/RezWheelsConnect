de5aa038964e9df655733e3e3562c5f2a64f3cc7
NULL <elowrey@unm.edu>
2024-11-11 10:58:33 -0700

    Create tracker.js

---
+++ backend/node/tracker.js
@@ -0,0 +1,2 @@
+// This script will be used as a tracker, recieves IP addresses from the recieve_ip_addr endpoint, and shares these IP addresses in a list.
+// Note that this will be used to create a safe p2p connection between devices.
